b010feb98c3fc81e2bd9f22d257cbef2e962b966
Ankit-kumar003 <ankitbhalothia3161@gmail.com>
2026-06-19 01:42:04 +0530

    fixed config to read password from environment

--- config.py
+++ config.py
@@ -4,17 +4,21 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'genxcafe-secret-key')
-    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads')
     
-    # Cloud Aiven Database ke credentials yahan daalein
-    MYSQL_HOST = 'mysql-98e4b7a-jangirrahul0026-386c.i.aivencloud.com'
-    MYSQL_PORT = 24240
-    MYSQL_USER = 'avnadmin'
-    MYSQL_DB = 'genxcafe'
-    # 'Click to reveal password' wale text par click karke jo password mila wo yahan copy karein
-    MYSQL_PASSWORD = ''
+    # 1. Database Configuration (Render environment variables se read karega)
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'mysql-98e4b7a-jangirrahul0026-386c.i.aivencloud.com')
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 24240))
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'avnadmin')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'genxcafe')
+    
+    # SYSTEM CORRECTED: Yeh line ab Render dashboard wale password ko fetch karegi
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    
+    # 2. Chatbot & Extra Configurations
     GROQ_API_KEY = os.environ.get('GROQ_API_KEY', '')
     GROQ_MODEL = 'llama3-70b-8192'
+    
+    # 3. File Uploads Config
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
     WTF_CSRF_ENABLED = True
